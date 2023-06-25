f8dfa24c4734b603d35d346598f98ffc46e13c67
LAPTOP-23C0I0R6\cdoky <cdokyung@gmail.com>
2023-06-25 01:41:24 -0700

    1

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,6 +1,7 @@
-export const base_url =
-    "https://e-commerce-server-c4scu1nbx-cory-kim.vercel.app/api/";
+export const base_url = "https://e-commerce-server-coral.vercel.app/api/";
 
 // "http://localhost:5000/api/";
 // "https://e-commerce-server-coral.vercel.app/api/";
 // "https://e-commerce-server-c4scu1nbx-cory-kim.vercel.app/api/";
+
+// https://e-commerce-server-coral.vercel.app/
