4f685a4de0e33c90d94dc6960de4199c62ba6607
LAPTOP-23C0I0R6\cdoky <cdokyung@gmail.com>
2023-06-25 01:10:37 -0700

    changed base url

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,3 +1,6 @@
-export const base_url = "https://e-commerce-server-coral.vercel.app/api/";
+export const base_url =
+    "https://e-commerce-server-c4scu1nbx-cory-kim.vercel.app/api/";
 
 // "http://localhost:5000/api/";
+// "https://e-commerce-server-coral.vercel.app/api/";
+// "https://e-commerce-server-c4scu1nbx-cory-kim.vercel.app/api/";
